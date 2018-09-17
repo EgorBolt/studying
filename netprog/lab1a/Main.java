42614bd66f4d5f53acd6a0b9fe56991417b5d58b
Egor Boldyrev <boldyreved@gmail.com>
2018-09-17 18:08:17 +0700

    Still testing.

--- netprog/lab1a/Main.java
+++ netprog/lab1a/Main.java
@@ -1,18 +1,16 @@
 import ru.nsu.fit.g16201.Boldyrev.Receive;
 import ru.nsu.fit.g16201.Boldyrev.Send;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        Receive receiver = new Receive();
+        Receive receiver = new Receive(args[0], Integer.parseInt(args[1]));
         Send sender = new Send(args[0], Integer.parseInt(args[1]));
         Scanner in = new Scanner(System.in);
         String message;
 
         receiver.start();
         sender.start();
-
     }
 }
--- netprog/lab1a/ru/nsu/fit/g16201/Boldyrev/Receive.java
+++ netprog/lab1a/ru/nsu/fit/g16201/Boldyrev/Receive.java
@@ -4,27 +4,46 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class Receive extends Thread {
-    protected MulticastSocket multicastSocket = null;
-    protected byte[] message = new byte[256];
+    private MulticastSocket multicastSocket;
+    private InetAddress multicastGroup;
+    private ArrayList<InetAddress> listAddress;
+
+    public Receive(String ipAddress, int port) {
+        try {
+            multicastSocket = new MulticastSocket(port);
+            multicastGroup = InetAddress.getByName(ipAddress);
+            listAddress = new ArrayList<>();
+        } catch (UnknownHostException eUnknownHost) {
+            eUnknownHost.printStackTrace();
+        } catch (IOException eIO) {
+            eIO.printStackTrace();
+        }
+    }
 
     public void run() {
         try {
-            multicastSocket = new MulticastSocket(5000);
-            InetAddress multicastGroup = InetAddress.getByName("235.0.0.0");
+            byte[] message;
+            String msg = "Hi!";
+            message = msg.getBytes();
             multicastSocket.joinGroup(multicastGroup);
-            while(true) {
+
+
+            while (true) {
                 DatagramPacket packet = new DatagramPacket(message, message.length);
                 multicastSocket.receive(packet);
+                InetAddress address = packet.getAddress();
+                if (!(listAddress.contains(address))) {
+                    listAddress.add(address);
+                }
+                System.out.println(packet.getAddress());
                 String received = new String(packet.getData(), 0, packet.getLength());
                 System.out.println(received);
-                if ("end".equals(received)) {
-                    break;
-                }
             }
-            multicastSocket.leaveGroup(multicastGroup);
-            multicastSocket.close();
+            //multicastSocket.close();
         } catch (IOException eIO) {
             eIO.printStackTrace();
         }
